df9f30694f4cdbb1d8c15d800317fea777918250
almasterhamzawy <alkberhamzay@gmail.com>
2019-10-05 13:02:13 +0200

    create full dashboard menu

--- resources/views/team/create.blade.php
+++ resources/views/team/create.blade.php
@@ -9,29 +9,65 @@
             <!-- /.box-header -->
             <!-- form start -->
             <form role="form" action="{{route('team.store')}}" method="post">
+                
+              @csrf  
               <div class="box-body">
                   
-                <div class="form-group">
-                  <label for="exampleInputEmail1">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                <div class="form-group has-feedback {{ $errors->has('first_name') ? 'has-error' : '' }}">
+                    <input type="text" name="first_name" class="form-control" value="{{ old('first_name') }}"
+                           placeholder="{{ trans('adminlte::adminlte.first_name') }}">
+                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                    @if ($errors->has('first_name'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('first_name') }}</strong>
+                        </span>
+                    @endif
                 </div>
-                  
-                <div class="form-group">
-                  <label for="exampleInputPassword1">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                <div class="form-group has-feedback {{ $errors->has('last_name') ? 'has-error' : '' }}">
+                    <input type="text" name="last_name" class="form-control" value="{{ old('last_name') }}"
+                           placeholder="{{ trans('adminlte::adminlte.last_name') }}">
+                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                    @if ($errors->has('name'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('last_name') }}</strong>
+                        </span>
+                    @endif
                 </div>
-                <div class="form-group">
-                  <label for="exampleInputFile">File input</label>
-                  <input type="file" id="exampleInputFile">
-
-                  <p class="help-block">Example block-level help text here.</p>
+                <div class="form-group has-feedback {{ $errors->has('email') ? 'has-error' : '' }}">
+                    <input type="email" name="email" class="form-control" value="{{ old('email') }}"
+                           placeholder="{{ trans('adminlte::adminlte.email') }}">
+                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                    @if ($errors->has('email'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
                 </div>
-
-              </div>
-              <!-- /.box-body -->
+                <div class="form-group has-feedback {{ $errors->has('password') ? 'has-error' : '' }}">
+                    <input type="password" name="password" class="form-control"
+                           placeholder="{{ trans('adminlte::adminlte.password') }}">
+                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                    @if ($errors->has('password'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                </div>
+                <div class="form-group has-feedback {{ $errors->has('password_confirmation') ? 'has-error' : '' }}">
+                    <input type="password" name="password_confirmation" class="form-control"
+                           placeholder="{{ trans('adminlte::adminlte.retype_password') }}">
+                    <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+                    @if ($errors->has('password_confirmation'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('password_confirmation') }}</strong>
+                        </span>
+                    @endif
+                </div>
 
               <div class="box-footer">
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary btn-block btn-flat">
+                    {{ trans('adminlte::adminlte.register') }}
+                </button>
               </div>
             </form>
           </div>
